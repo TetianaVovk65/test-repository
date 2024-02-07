e53065664bb1d791c3ab9a4c0f2763d11c5eb45b
TetianaVovk65 <johnbrook656@gmail.com>
2024-02-07 17:39:59 +0200

    Task1

---
+++ Homework8.py
@@ -0,0 +1,9 @@
+# task1
+September = {"Ivanov", "Shevchenko", "Popli", "Lidia"}
+October = {"Oser", "Ivanov", "Serb", "Shevchenko"}
+both = September.intersection(October)
+only_october = October.difference_update(September)
+print(both)
+print(October)
+
+#task2
