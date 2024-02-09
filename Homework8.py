6850b8324608c52454732cec3b4590b8192babb4
TetianaVovk65 <johnbrook656@gmail.com>
2024-02-09 10:17:12 +0200

    Task1

--- Homework8.py
+++ Homework8.py
@@ -6,4 +6,21 @@ only_october = October.difference_update(September)
 print(both)
 print(October)
 
-#task2
+# task2
+permissions = {}
+n = int(input('Put the number of files: '))
+for _ in range(n):
+    s = input().split()
+    permissions[s[0]] = set(s[1:])
+for _ in range(int(input('Put the number of files for checking: '))):
+    perm, file = input().split()
+    if perm == 'read':
+        perm = 'R'
+    if perm == 'write':
+        perm = 'W'
+    if perm == 'execute':
+        perm = 'X'
+    if perm in permissions[file]:
+        print('OK')
+    else:
+        print('Access denied')
